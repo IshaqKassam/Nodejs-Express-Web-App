36f191a6ee2909b2a6aa7ee55fbc6c5f1cc3a755
Ishaq Kassam <ishaq.kassam@gmail.com>
2021-11-10 16:26:44 +0300

    setup debug tools

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const chalk = require('chalk') //used to color texts - useful in console logging colored messages
+// const debug = require('debug')('app')
+const morgan = require('morgan')
 
 const app = express()
 
+app.use(morgan('tiny'))
 
 app.get('/', (req, res) => {
     res.send("hello from my app")
 })
 
 app.listen(3000, () => {
-    console.log("Listening on Port" + chalk.green(' 3000'))
+    console.log(`Listening on Port ${chalk.green(' 3000')}`)
 })
